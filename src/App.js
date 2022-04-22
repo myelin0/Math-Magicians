c0065a7f426b349aaf41a7b99ec93c239160ad13
myelin0 <emi.veisllari@gmail.com>
2022-04-22 11:39:42 +0200

    Refactor Calculator using function component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    return <Calculator />;
-  }
-}
+const App = () => <Calculator />;
 
 export default App;
